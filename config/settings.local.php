2745745ed0562a10ab2f0d3fab4b69a7c4381718
Lasha Dolidze <lashad@mac.com>
2024-01-30 15:48:46 +0400

    Fix issue

--- config/settings.local.php
+++ config/settings.local.php
@@ -4,6 +4,8 @@ if (!isset($databases)) {
     $databases = array();
 }
 
+$settings['hash_salt'] = 'tdGHVg8dVRiiGGz3q5OFKrq1tDVaibvrH-2z2ro_U2QlUU8Oi4aksXqbgZwuic9rP6upB1-fBw';
+
 $databases['default']['default'] = array(
     'driver' => 'mysql',
     'database' => 'pantheon',
